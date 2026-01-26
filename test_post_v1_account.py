9f035c590869b5b684101a10a3f1fa70f191dafa
Battle_Hamster <christy.lubchuk@gmail.com>
2026-01-26 23:05:47 +0100

    [DM-API] /account test with auto activation

--- test_post_v1_account.py
+++ test_post_v1_account.py
@@ -2,15 +2,16 @@ import pprint
 
 import pytest
 import requests
+from json import loads
 
 
 def test_post_v1_account():
 
-    login = 'bhs-test'
+    login = 'bhs-test3'
     password ='123456789'
     email = f'{login}@test'
 
-    # 1. Регистрация пользователя
+    # # 1. Регистрация пользователя
     json_data = {
         'login': login,
         'email': email,
@@ -18,9 +19,10 @@ def test_post_v1_account():
     }
 
     response = requests.post('http://185.185.143.231:5051/v1/account', json=json_data)
-    # 2.1 Получить письмо из почтового сервера
     print(response.status_code)
-    print(response.text)
+    # print(response.text)
+
+    # # 2.1 Получить письмо из почтового сервера
 
     params = {
         'limit': '50',
@@ -28,19 +30,36 @@ def test_post_v1_account():
 
     response = requests.get('http://185.185.143.231:5025/api/v2/messages', params=params, verify=False)
     print(response.status_code)
-    print(response.text)
+    #print(response.text)
 
     # 2.2 Получить активационный токен
+    #pprint.pprint(response.json())
+    for item in response.json()['items']:
+        body  = item['Content']['Body']
+        if body.startswith('{'):
+            try:
+                user_data = loads(body)
+
+                user_login = user_data['Login']
+
+                if user_login == login:
+                    token = user_data['ConfirmationLinkUrl'].split('/')[-1]
+                    print(token)
+
+            except (ValueError, KeyError):
+                pass
+
+
 
     # 2. Активировать пользователя
     headers = {
         'accept': 'text/plain',
     }
 
-    response = requests.put('http://185.185.143.231:5051/v1/account/76b12015-71a8-4f17-9e8c-b3a7cd7bc569',
+    response = requests.put(f'http://185.185.143.231:5051/v1/account/{token}',
                             headers=headers)
     print(response.status_code)
-    print(response.text)
+    # print(response.text)
 
     # 3. Авторизоваться
 
@@ -52,5 +71,4 @@ def test_post_v1_account():
 
     response = requests.post('http://185.185.143.231:5051/v1/account/login', json=json_data)
     print(response.status_code)
-    print(response.text)
-    pass
+    # print(response.text)
