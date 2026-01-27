b9d12ba9d8b45334b62d0fb1f431debbf42a947e
Battle_Hamster <christy.lubchuk@gmail.com>
2026-01-28 00:42:35 +0100

    [DM-API] Added test and APIs structure. Moved functions to Client classes

---
+++ dm_api_account/apis/login_api.py
@@ -0,0 +1,29 @@
+import requests
+
+
+class LoginApi:
+    def __init__(
+            self,
+            host,
+            headers=None
+    ):
+        self.host = host
+        self.email = headers
+
+    def post_v1_account_login(
+            self,
+            json_data
+            ):
+        """
+        Authenticate via credentials
+        :param json_data:
+        :return:
+        """
+        # json_data = {
+        #     'login': login,
+        #     'password': password,
+        #     'rememberMe': True,
+        # }
+        response = requests.post(url=f'{self.host}/v1/account/login',
+                                 json=json_data)
+        return response
