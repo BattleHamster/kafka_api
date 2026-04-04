dbc7f5170093fcc4183b1a1f49139fe323adfaeb
Battle_Hamster <christy.lubchuk@gmail.com>
2026-04-05 00:10:25 +0200

    [DECORATOR] Retrier decorator added

--- tests/functional/post_v1_account/test_post_v1_account.py
+++ tests/functional/post_v1_account/test_post_v1_account.py
@@ -31,12 +31,11 @@ def test_post_v1_account():
 
     account_helper = AccountHelper(dm_account_api=account, mailhog=mailhog)
 
-    login = 'bhs-test13'
+    login = 'bhs-test18'
     password ='123456789'
     email = f'{login}@test'
 
     account_helper.register_new_user(login=login, password=password, email=email)
-
     account_helper.user_login(login=login, password=password)
 
     # 1. Регистрация пользователя
